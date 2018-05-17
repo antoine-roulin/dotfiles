dee2f0b962cab879bd13d762400d16a1399e86b9
Antoine Roulin <antoine.roulin@eisti.fr>
2018-05-17 10:53:55 +0200

    fish PATH params

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 set PATH ~/anaconda3/bin $PATH
 set PATH ~/.cargo/bin $PATH
+set PATH /usr/local/go/bin $PATH
+set PATH ~/go/bin $PATH
+set PATH ~/android-studio/bin $PATH
+set PATH ~/Android/Sdk/platform-tools $PATH
+set PATH ~/Android/Sdk/tools $PATH
+set PATH ~/android-studio/gradle/gradle-4.4/bin $PATH
 
 alias ll="ls --human-readable -l"
 
