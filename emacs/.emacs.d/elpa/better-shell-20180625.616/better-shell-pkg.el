8afe218a07f511105b837394afbebf836a3f7874
Antoine Roulin <antoine.roulin@eisti.fr>
2018-10-17 18:47:38 +0200

    update emacs

--- emacs/.emacs.d/elpa/better-shell-20180625.616/better-shell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "better-shell" "20180625.616" "Better shell management" '((emacs "24.4")))
--- emacs/.emacs.d/elpa/popup-20160709.1429/popup-autoloads.el
+++ emacs/.emacs.d/elpa/popup-20160709.1429/popup-autoloads.el
@@ -3,6 +3,12 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
+;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/popup-20160709.1429/popup-autoloads.el"
+;;;;;;  "../../../../../.emacs.d/elpa/popup-20160709.1429/popup.el")
+;;;;;;  (23495 25743 395335 259000))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
