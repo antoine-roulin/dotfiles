8e3a75f97dbfd1f4219a080ec68752911b83f4fd
Antoine Roulin <antoine.roulin@eisti.fr>
2019-02-06 16:16:41 +0100

    updated gradle PATH, locale and opam init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,9 @@ set PATH ~/go/bin $PATH
 set PATH ~/android-studio/bin $PATH
 set PATH ~/Android/Sdk/platform-tools $PATH
 set PATH ~/Android/Sdk/tools $PATH
-set PATH ~/android-studio/gradle/gradle-4.4/bin $PATH
+set PATH ~/android-studio/gradle/gradle-4.10.1/bin $PATH
+
+set LC_CTYPE fr_FR.UTF-8
 
 alias ll="ls --human-readable -l"
 
@@ -16,3 +18,5 @@ function bibOfDoi --description 'Fetch the BibTeX entry corresponding to a DOI'
     curl -LH 'Accept: application/x-bibtex' $argv
 end
 
+# opam configuration
+source /home/antoine/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
