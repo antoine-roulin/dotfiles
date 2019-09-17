d980c9e3ba999f45fcff8120c4c45bba53a9b290
Antoine Roulin <antoine.roulin@eisti.fr>
2019-09-17 12:44:49 +0200

    minimal emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,23 +1,46 @@
-(setq user-full-name "Antoine Roulin"
-      user-mail-address "antoine.roulin@eisti.fr")
+;; -*- lexical-binding: t; -*-
 
-;; GUI
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(when (member "Iosevka" (font-family-list)) (set-frame-font "Iosevka-13" t t))
+
+(setq-default use-package-always-defer t
+              use-package-always-ensure t
+              indent-tabs-mode nil
+              tab-width 2
+              css-indent-offset 2)
+
+(desktop-save-mode 1)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq make-backup-files nil
+      auto-save-default nil
+      inhibit-splash-screen t
+      visible-bell nil)
+
+(setq user-full-name "Antoine Roulin"
+      user-mail-address "work.roulin@gmail.com")
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(tooltip-mode -1)
-(scroll-bar-mode -1)
-
-(toggle-frame-maximized)
-
-(fset 'yes-or-no-p 'y-or-n-p) ; shorter messages
-
-(global-set-key (kbd "<f5>") 'revert-buffer) ; reload a file in the current buffer
 
 (global-hl-line-mode t) ; highlight current line
 
 (setq-default frame-title-format "%b (%f)") ; filename in title bar
-
-(setq ring-bell-function 'ignore) ; no bell
 
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
@@ -29,60 +52,55 @@
 
 (electric-pair-mode 1) ; auto bracket insertion
 
-(c-set-offset (quote cpp-macro) 0 nil) ; pragma indentation in C/C++
-
-;; Backup and auto-saved files in temporary directory
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; Font and display
-(set-face-attribute 'default t :font "Source Code Pro" :height 126)
-(set-face-attribute 'variable-pitch t :font "Charter" :height 144)
-
-(defun set-buffer-variable-pitch ()
-  (interactive)
-  (variable-pitch-mode t)
-  (setq line-spacing 3)
-  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-link nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-date nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-special-keyword nil :inherit 'fixed-pitch))
-
-(add-hook 'org-mode-hook 'set-buffer-variable-pitch)
-(add-hook 'eww-mode-hook 'set-buffer-variable-pitch)
-(add-hook 'markdown-mode-hook 'set-buffer-variable-pitch)
-(add-hook 'Info-mode-hook 'set-buffer-variable-pitch)
-
 (setq org-hide-emphasis-markers t)
 
 (global-prettify-symbols-mode 1)
 (setq prettify-symbols-unprettify-at-point t)
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
-(package-initialize)
+(setq ring-bell-function (lambda ()
+                           (invert-face 'mode-line)
+                           (run-with-timer 0.1 nil 'invert-face 'mode-line)))
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+(setq exec-path (append exec-path '("/usr/local/bin")))
 
-(setq use-package-always-ensure t)
+(use-package flycheck
+  :defer 1
+  :init (global-flycheck-mode))
 
-;; Provides help for keys
+(use-package projectile
+  :config (progn
+            (projectile-mode)
+            (setq projectile-enable-caching t)
+            (setq projectile-completion-system 'ivy)))
+
+(use-package counsel-projectile
+  :config
+  (counsel-projectile-mode))
+
+(use-package magit
+  :pin melpa-stable
+  :defer 1
+  :config (progn
+            (put 'magit-clean 'disabled nil)
+            (add-hook 'magit-status-sections-hook 'magit-insert-worktrees)
+            (setq magit-commit-show-diff nil)))
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc")
+  :config (progn
+            (add-hook 'markdown-mode-hook 'visual-line-mode)
+            (add-hook 'markdown-mode-hook (lambda () (flyspell-mode 1)))))
+
 (use-package which-key
+  :defer 1
   :init
   (setq which-key-separator " ")
   (setq which-key-prefix-prefix "+")
-  :config
-  (which-key-mode 1))
+  :config (which-key-mode))
 
 ;; A better help buffer
 (use-package helpful)
@@ -94,55 +112,40 @@
 (setq org-src-tab-acts-natively t)
 (setq org-src-fontify-natively t)
 
-;; Window switching
-(use-package ace-window
-  :init
-  (global-set-key [remap other-window] 'ace-window)
-  (custom-set-faces
-   '(aw-leading-char-face
-     ((t (:inherit ace-jump-face-foreground :height 3.0))))))
+(setq dired-dwim-target t
+      dired-recursive-deletes t
+      dired-use-ls-dired nil
+      delete-by-moving-to-trash t)
 
-;; Search
-(use-package swiper
-  :ensure counsel
-  :ensure try
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "<f6>") 'ivy-resume)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l") 'counsel-load-library)
-  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
-    
-;; Quick navigation
-(use-package avy
-  :bind ("M-s" . avy-goto-char))
-  
-;; Auto-completion
-(use-package auto-complete
-  :init
-  (ac-config-default)
-  (global-auto-complete-mode t))
+(use-package editorconfig
+  :defer 1
+  :config (editorconfig-mode 1))
 
-;; Snippets
-(use-package yasnippet
-  :init
-  (yas-global-mode 1))
+(use-package flyspell
+  :defer 1
+  :config (progn
+            (setq flyspell-issue-message-flag nil)))
+
+(use-package ivy
+  :defer 1
+  :config (progn
+            (ivy-mode)
+            (setq ivy-use-virtual-buffers t)
+            (setq ivy-count-format "")
+            (setq ivy-use-selectable-prompt t)))
+
+(use-package counsel
+  :defer 1
+  :config (progn
+            (global-set-key (kbd "M-x") 'counsel-M-x)))
+
+(use-package swiper :defer 1)
+
+(use-package vlf)
 
 ;; Solarized theme
 (use-package solarized-theme
+  :defer 0.1
   :custom
   (solarized-distinct-fringe-background t)
   (solarized-high-contrast-mode-line t)
@@ -151,50 +154,11 @@
   :config
   (load-theme 'solarized-light t))
 
-;; Linting
-(use-package flycheck
-  :init
-  (global-flycheck-mode t))
-
 ;; Quickly delete whitespace
 (use-package hungry-delete
+  :defer 1
   :config
   (global-hungry-delete-mode))
-
-;; Expand the marked region by semantic units
-(use-package expand-region
-  :config
-  (global-set-key (kbd "C-=") 'er/expand-region))
-
-(use-package better-shell
-  :bind
-  (("C-'" . better-shell-shell)
-   ("C-;" . better-shell-remote-open)))
-
-;; Project management
-(use-package projectile
-  :config
-  (projectile-mode)
-  (setq projectile-completion-system 'ivy)
-  (setq projectile-enable-caching t))
-
-(use-package counsel-projectile
-  :config
-  (counsel-projectile-mode))
-
-;; startup screen
-(use-package dashboard
-  :ensure page-break-lines
-  :config
-  (dashboard-setup-startup-hook)
-  (setq dashboard-banner-logo-title "Praise St. IGNUcius!")
-  (setq dashboard-startup-banner "~/dotfiles/emacs/.emacs.d/top_gnu.jpeg")
-  (setq dashboard-image-banner-max-width 500)
-  (setq dashboard-items '((recents . 5)
-			  (projects . 5))))
-
-;; Git versionning
-(use-package magit)
 
 ;; LaTeX editing
 (use-package tex
@@ -207,56 +171,13 @@
   :config
   (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f")))
 
-;; Racket programming
-(use-package racket-mode
-  :hook (((racket-mode racket-repl-mode) . racket-unicode-input-method-enable)
-	 (racket-mode . (lambda ()
-			  (define-key racket-mode-map (kbd "C-c r") 'racket-run)))))
-
-;; Markdown
-(use-package markdown-mode
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "pandoc"))
-
-;; nice writing environment
-(use-package olivetti
-  :ensure t
-  :hook ((org-mode LaTeX-mode text-mode) . olivetti-mode))
-
-;; multiple cursors
-;;(use-package multiple-cursors
-  ;;:ensure t
-  ;;:config
-  ;;(global-unset-key (kbd "M-<down-mouse-1>"))
-  ;;(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click))
-
-;; Scala
-(use-package scala-mode
-  :interpreter ("scala" . scala-mode)
-  :config
-  (setq prettify-symbols-alist scala-prettify-symbols-alist))
-
-(use-package eldoc
-  :ensure nil
-  :diminish eldoc-mode
-  :commands eldoc-mode)
-
-(use-package elixir-mode
-  :mode "\\.elixir2\\'")
-
-(provide 'init)
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (org-ref helpful org dashboard page-break-lines scala-mode elixir-mode yasnippet which-key web-mode use-package try solarized-theme racket-mode org-journal org-bullets olivetti markdown-mode magit jedi hungry-delete flycheck expand-region emmet-mode counsel-projectile better-shell auctex ace-window)))
+   '(org-ref auctex page-break-lines hungry-delete solarized-theme vlf editorconfig org-bullets helpful which-key markdown-mode magit counsel-projectile projectile flycheck use-package))
  '(solarized-distinct-fringe-background t)
  '(solarized-high-contrast-mode-line t)
  '(solarized-use-more-italic t)
@@ -266,14 +187,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro" :height 1.5 :underline nil))))
- '(org-level-1 ((t (:inherit default :foreground "black" :slant normal :weight bold :height 1.75 :width normal :foundry "ADBO" :family "Charter"))))
- '(org-level-2 ((t (:inherit default :foreground "black" :slant normal :weight bold :height 1.5 :width normal :foundry "ADBO" :family "Charter"))))
- '(org-level-3 ((t (:inherit default :foreground "black" :slant normal :weight bold :height 1.25 :width normal :foundry "ADBO" :family "Charter"))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :font "Source Sans Pro"))))
- '(variable-pitch ((t (:family "Charter")))))
+ )
