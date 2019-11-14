7248f17cf5c6c477f676964b22c696b917313290
Antoine Roulin <antoine.roulin@coopengo.com>
2019-11-14 11:23:48 +0100

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,49 +1,35 @@
-;; -*- lexical-binding: t; -*-
+;; Setup
+
+(setq user-full-name "Antoine Roulin"
+      user-mail-address "antoine.roulin@coopengo.com")
 
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
-
-(when (member "Iosevka SS01" (font-family-list)) (set-frame-font "Iosevka SS01-13" t t))
-
-(setq-default use-package-always-defer t
-              use-package-always-ensure t
-              indent-tabs-mode nil
-              tab-width 2
-              css-indent-offset 2)
-
-(desktop-save-mode 1)
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(setq make-backup-files nil
-      auto-save-default nil
-      inhibit-splash-screen t
-      visible-bell nil)
-
-(setq user-full-name "Antoine Roulin"
-      user-mail-address "antoine.roulin@coopengo.com")
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-
-(global-hl-line-mode t) ; highlight current line
-
-(setq-default frame-title-format "%b (%f)") ; filename in title bar
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
+
+;; Interface
+
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-hl-line-mode t) ; highlight current line
+
+(setq-default frame-title-format "%b (%f)") ; filename in title bar
 
 ;; Highlights matching parenthesis
 (setq show-paren-delay 0)
@@ -51,131 +37,179 @@
 
 (electric-pair-mode 1) ; auto bracket insertion
 
-(setq org-hide-emphasis-markers t)
+(when (member "Iosevka SS01" (font-family-list)) (set-frame-font "Iosevka SS01-14" t t))
 
-(global-prettify-symbols-mode 1)
-(setq prettify-symbols-unprettify-at-point t)
-
-(setq ring-bell-function (lambda ()
-                           (invert-face 'mode-line)
-                           (run-with-timer 0.1 nil 'invert-face 'mode-line)))
-
-(setq exec-path (append exec-path '("/usr/local/bin")))
-
-(use-package flycheck
-  :defer 1
-  :init (global-flycheck-mode))
-
-(use-package projectile
-  :config (progn
-            (projectile-mode)
-            (setq projectile-enable-caching t)
-            (setq projectile-completion-system 'ivy)))
-
-(use-package counsel-projectile
-  :config
-  (counsel-projectile-mode))
-
-(use-package magit
-  :pin melpa-stable
-  :defer 1
-  :config (progn
-            (put 'magit-clean 'disabled nil)
-            (add-hook 'magit-status-sections-hook 'magit-insert-worktrees)
-            (setq magit-commit-show-diff nil)))
-
-(use-package markdown-mode
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "pandoc")
-  :config (progn
-            (add-hook 'markdown-mode-hook 'visual-line-mode)
-            (add-hook 'markdown-mode-hook (lambda () (flyspell-mode 1)))))
-
-(use-package which-key
-  :defer 1
-  :init
-  (setq which-key-separator " ")
-  (setq which-key-prefix-prefix "+")
-  :config (which-key-mode))
-
-;; A better help buffer
-(use-package helpful)
-
-;; Nicer bullets for org-mode
-(use-package org-bullets
-  :hook (org-mode . org-bullets-mode))
-;; Org-mode code blocks
-(setq org-src-tab-acts-natively t)
-(setq org-src-fontify-natively t)
-
-(setq dired-dwim-target t
-      dired-recursive-deletes t
-      dired-use-ls-dired nil
-      delete-by-moving-to-trash t)
-
-(use-package editorconfig
-  :defer 1
-  :config (editorconfig-mode 1))
-
-(use-package flyspell
-  :defer 1
-  :config (progn
-            (setq flyspell-issue-message-flag nil)))
-
-(use-package ivy
-  :defer 1
-  :config (progn
-            (ivy-mode)
-            (setq ivy-use-virtual-buffers t)
-            (setq ivy-count-format "")
-            (setq ivy-use-selectable-prompt t)))
-
-(use-package counsel
-  :defer 1
-  :config (progn
-            (global-set-key (kbd "M-x") 'counsel-M-x)))
-
-(use-package swiper :defer 1)
-
-(use-package vlf)
-
-;; Solarized theme
 (use-package solarized-theme
-  :defer 0.1
+  :ensure t
   :custom
+  (solarized-use-variable-pitch nil)
+  (solarized-scale-org-headlines nil)
   (solarized-distinct-fringe-background t)
-  (solarized-high-contrast-mode-line t)
+  (solarized-high-contrast-mode-line nil)
   (solarized-use-more-italic t)
   (x-underline-at-descent-line t)
   :config
   (load-theme 'solarized-light t))
 
-;; Quickly delete whitespace
+(use-package doom-modeline
+  :ensure t)
+(doom-modeline-mode)
+
+(setq save-interprogram-paste-before-kill t)
+
+(desktop-save-mode 1)
+
 (use-package hungry-delete
-  :defer 1
+  :ensure t
   :config
   (global-hungry-delete-mode))
 
-(use-package auto-complete
+(use-package aggressive-indent
+  :ensure t
+  :config
+  (global-aggressive-indent-mode 1))
+
+;; Swiper, Ivy, Counsel
+
+(use-package counsel
+  :ensure t
+  :bind
+  (("M-y" . counsel-yank-pop)
+   :map ivy-minibuffer-map
+   ("M-y" . ivy-next-line)))
+
+(use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "%d/%d ")
+  (setq ivy-display-style 'fancy))
+
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper-isearch)
+	 ("C-r" . swiper-isearch)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)))
+
+;; Linter
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+
+;; Small interface tweaks
+
+(use-package which-key
+  :ensure t 
+  :config
+  (which-key-mode))
+
+(use-package helpful
+  :ensure t)
+
+;; Magit
+
+(use-package magit
   :ensure t
   :init
   (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)))
+    (bind-key "C-x g" 'magit-status)))
 
-;; Python Config
+(setq magit-status-margin
+      '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18))
 
-(setq py-python-command "python3")
-(setq python-shell-interpreter "python3")
+(use-package git-timemachine
+  :ensure t)
 
-(use-package jedi
+(use-package git-gutter
   :ensure t
-  :hook ((python-mode-hook . jedi:setup)
-         (python-mode-hook . jedi:ac-setup))
-  :config (setq jedi:complete-on-dot t))
+  :init
+  (global-git-gutter-mode +1))
+
+;; Org-mode
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+
+(setq org-hide-emphasis-markers t)
+(setq org-src-tab-acts-natively t)
+(setq org-src-fontify-natively t)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((js . t)))
+
+;; Parens
+
+(use-package smartparens
+  :ensure t
+  :hook (prog-mode . smartparens-mode)
+  :custom
+  (sp-escape-quotes-after-insert nil)
+  :config
+  (require 'smartparens-config))
+
+(show-paren-mode t)
+
+;; Snippets
+
+(use-package yasnippet
+  :ensure t
+  :init
+  (yas-global-mode 1))
+(use-package yasnippet-snippets
+  :ensure t)
+
+;; Projectile
+
+(use-package projectile
+  :ensure t
+  :bind (:map projectile-mode-map
+              ("C-c p" . 'projectile-command-map))
+  :config 
+  (projectile-mode +1))
+
+;; Completion
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (global-company-mode t))
+
+;; Python
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+            (setq indent-tabs-mode nil)
+	    (infer-indentation-style)
+	    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
+(use-package virtualenvwrapper
+  :ensure t
+  :config
+  (venv-initialize-interactive-shells)
+  (venv-initialize-eshell))
+
+(venv-workon "coog")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -184,10 +218,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (elpy page-break-lines hungry-delete solarized-theme vlf editorconfig org-bullets helpful which-key markdown-mode magit counsel-projectile projectile flycheck use-package)))
- '(solarized-distinct-fringe-background t t)
- '(solarized-high-contrast-mode-line t t)
- '(solarized-use-more-italic t t)
+    (helpful virtualenvwrapper company projectile yasnippet-classic-snippets yasnippet-snippets yasnippet smartparens org-bullets git-gutter git-timemachine magit which-key flycheck counsel aggressive-indent hungry-delete doom-modeline solarized-theme use-package)))
+ '(solarized-distinct-fringe-background t)
+ '(solarized-high-contrast-mode-line nil)
+ '(solarized-use-more-italic t)
+ '(sp-escape-quotes-after-insert nil)
  '(x-underline-at-descent-line t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
