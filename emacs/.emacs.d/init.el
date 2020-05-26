80c8068cc9e55927d3fc4f019aced634d04b9b8a
Antoine Roulin <antoine.roulin@coopengo.com>
2020-05-26 12:21:12 +0200

    Clean emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,15 +14,14 @@
 (require 'package)
 
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 ;; UTF-8 as default encoding
 (set-language-environment "UTF-8")
@@ -31,13 +30,15 @@
 ;; Interface
 
 (setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
+(scroll-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (global-hl-line-mode t) ; highlight current line
 
-(setq-default frame-title-format "%b (%f)") ; filename in title bar
+(setq-default frame-title-format "%f") ; filename in title bar
 
 ;; Highlights matching parenthesis
 (setq show-paren-delay 0)
@@ -45,7 +46,7 @@
 
 (electric-pair-mode 1) ; auto bracket insertion
 
-(when (member "Iosevka SS01" (font-family-list)) (set-frame-font "Iosevka SS01-14" t t))
+(when (member "Iosevka SS01" (font-family-list)) (set-frame-font "Iosevka SS01-12" t t))
 
 (use-package solarized-theme
   :ensure t
@@ -63,35 +64,9 @@
 
 (desktop-save-mode 1)
 
-(use-package hungry-delete
-  :ensure t
-  :hook prog-mode)
-
-(use-package aggressive-indent
-  :ensure t
-  :hook prog-mode)
-
-;; Helm
-
-(use-package helm
-  :ensure t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x f" . helm-recentf)
-         ("C-SPC" . helm-dabbrev)
-         ("M-y" . helm-show-kill-ring)
-         ("C-x b" . helm-mini))
-  :config
-  (helm-mode 1)
-  (setq helm-buffers-fuzzy-matching t
-        helm-M-x-fuzzy-match t
-        helm-recentf-fuzzy-match t))
-
-;; Linter
-
-(use-package flycheck
-  :ensure t
-  :hook prog-mode)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
 ;; Small interface tweaks
 
@@ -137,31 +112,6 @@
       org-src-fontify-natively t
       org-catch-invisible-edits 'show-and-error)
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((js . t)))
-
-;; Parens
-
-(use-package smartparens
-  :ensure t
-  :hook (prog-mode . smartparens-mode)
-  :custom
-  (sp-escape-quotes-after-insert nil)
-  :config
-  (require 'smartparens-config))
-
-(show-paren-mode t)
-
-;; Snippets
-
-(use-package yasnippet
-  :ensure t
-  :hook prog-mode)
-(use-package yasnippet-snippets
-  :ensure t
-  :hook yasnippet)
-
 ;; Projectile
 
 (use-package projectile
@@ -171,30 +121,20 @@
   :config
   (projectile-mode +1))
 
-;; Completion
-
-(use-package company
-  :ensure t
-  :hook prog-mode
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 3))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(doom-modeline-height 25)
  '(package-selected-packages
    (quote
-    (helpful company projectile yasnippet-classic-snippets yasnippet-snippets yasnippet smartparens org-bullets git-gutter git-timemachine magit which-key flycheck aggressive-indent hungry-delete solarized-theme use-package)))
+    (helpful projectile smartparens org-bullets git-gutter git-timemachine magit which-key solarized-theme use-package)))
  '(solarized-distinct-fringe-background t)
  '(solarized-high-contrast-mode-line nil)
  '(solarized-scale-org-headlines nil)
  '(solarized-use-more-italic t)
  '(solarized-use-variable-pitch nil)
- '(sp-escape-quotes-after-insert nil t)
+ '(sp-escape-quotes-after-insert nil)
  '(x-underline-at-descent-line t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
