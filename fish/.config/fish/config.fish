99432d280b239e8b65605872c98ad67acb6383ab
Antoine Roulin <antoine.roulin@coopengo.com>
2021-04-22 11:04:49 +0200

    Use default home path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 set PATH ~/bin ~/.local/bin $PATH
 set PATH ~/.emacs.d/bin $PATH
 
-set -x PATH "/home/coopengo/.pyenv/bin" $PATH
+set PATH ~/.pyenv/bin $PATH
 status --is-interactive; and pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
 
@@ -9,7 +9,6 @@ set -x DIRENV_LOG_FORMAT ""
 eval (direnv hook fish)
 
 set -gx FZF_DEFAULT_COMMAND 'fdfind --type f'
-set FZF_DEFAULT_OPTS '--color fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33
-		      --color info:136,prompt:136,pointer:234,marker:234,spinner:136'
+set FZF_DEFAULT_OPTS '--color fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33,info:136,prompt:136,pointer:234,marker:234,spinner:136'
 
-set -x PATH "/home/coopengo/git-fuzzy/bin" $PATH
+set PATH ~/git-fuzzy/bin $PATH
